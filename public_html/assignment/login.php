b4d8116e49f8ac448aba5b4f3b8c139c1eb14e51
dannybeales <s185384@ucs.ac.uk>
2018-11-14 15:11:51 +0000

    Added Login Form
    Working

--- public_html/assignment/login.php
+++ public_html/assignment/login.php
@@ -5,48 +5,18 @@ require_once("includes/headers/header.php");
 
 
 <?php 
-//Login proccess 
-
-	$username = filter_has_var(INPUT_POST, 'username') ? $_POST['username']: null;
-	$username = trim($username);
-	$password = filter_has_var(INPUT_POST, 'password') ? $_POST['password']: null;
-	$password = trim($password);
-
-	if (empty($username) || empty($password)) {
-		
-		echo "<p>You need to provice a username AND password. Please try again</p>";
-	}
-	else {
-		try {
-			
-			//first we need to clear any sessions that may have already existed.
-			unset($_SESSION['username']);
-			unset($_SESSION['logged-in']);
-			
-			$dbConn = getConnection();
-			
-			$sqlQuery = "SELECT passwordHash FROM users WHERE username = :username";
-			$stmt = $dbConn->prepare($sqlQuery);
-			$stmt->execute(array(':username' => $username));
-			$user = $stmt->fetchObject();
-		
-		} catch (Exception $e) {
-			echo "Record Not Found: " . $e->getMessage();
-			
-		}
-		
-	}
-			
-		
-		
-
+//Login
 
 
 
 
 ?>
 
-
+<form method="post" action="loginProccess.php">
+Username <input type="text" name="username" />
+		Password <input type="password" name="password" />
+<input type="submit" value="Logon" />
+</form>
 
 
 
