f23824bfc18089ea7f6034c659ded24095c60319
dannybeales <s185384@ucs.ac.uk>
2018-11-14 14:14:38 +0000

    Created the PHP for the Logon proccess

---
+++ public_html/assignment/loginProccess.php
@@ -0,0 +1,82 @@
+<?php 
+require_once("includes/headers/header.php");
+?>
+
+
+
+<?php 
+//Login proccess 
+
+	$username = filter_has_var(INPUT_POST, 'username') ? $_POST['username']: null;
+	$username = trim($username);
+	$password = filter_has_var(INPUT_POST, 'password') ? $_POST['password']: null;
+	$password = trim($password);
+
+	if (empty($username) || empty($password)) {
+		
+		echo "<p>You need to provide a username AND password. Please try again</p>";
+	}
+	else {
+		try {			
+			//first we need to clear any sessions that may have already existed.
+			unset($_SESSION['username']);
+			unset($_SESSION['logged-in']);
+			
+			$dbConn = getConnection();
+			
+			$sqlQuery = "SELECT passwordHash FROM users WHERE username = :username";
+			$stmt = $dbConn->prepare($sqlQuery);
+			$stmt->execute(array(':username' => $username));
+			$user = $stmt->fetchObject();
+			
+			//if there is a record
+			if($user) 
+			{					
+					if (password_verify($password, $user->passwordHash)) {
+						echo "<p>Logon Success!</p>\n";
+						echo "<p>Restricted Page<p>\n";
+						
+						$_SESSION['logged-in'] = true;
+						$_SESSION['username'] = $username;
+					}
+					else
+					{						
+						echo "<p>The username or password were incorrect. Please try again.</p>";					
+					}					
+			}
+			else
+			{						
+				echo "<p>The username or password were incorrect. Please try again.</p>";					
+			}
+			
+		} catch (Exception $e) {
+			echo "Record Not Found: " . $e->getMessage();
+			
+		}
+		
+	}
+			
+		
+		
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+
+require_once("includes/footer.php");
+
+?>
